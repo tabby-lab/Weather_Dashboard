165aad4e1f1a22e1deedaa994f6746c9b2e6c703
Tabby Garcia <tabby.garcia1@gmail.com>
2020-01-27 10:30:14 -0600

    links added. Header styled.

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+var i=0;
+var cityArr=[];
+var apiKey=ea3467aafccdf773b0cc5ec23211ed95;
+
+// Use the [OpenWeather API](https://openweathermap.org/api)
+//- http://api.openweathermap.org/data/2.5/weather
+//- http://api.openweathermap.org/data/2.5/forecast
+//- http://api.openweathermap.org/data/2.5/uvi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
