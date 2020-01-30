6a4c72dd042d7892275df777ff7f44fa617451ba
Tabby Garcia <tabby.garcia1@gmail.com>
2020-01-30 13:24:05 -0600

    Got my 5 day forecast up,still need local storage

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Weather_Dashboard
+This app is designed to show the user the current weather of a city.
+The user can search using the search bar, then the current city will provide the temperature,
+uv index, wind speed, and humidity.
+It will also allow the user to view a 5 day forecast for the city that they search.
+
+# Link
+Put github link here
+
+# Credit
+Calvin Carter, Amanda and Mark, Phil Loy, youtube, 
--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-    let i = 0;
+     let index = 0;
     let cityArr = [];
     const apiKey = "ea3467aafccdf773b0cc5ec23211ed95";
-
+    const convertTemp=temperature=> {
+        return Math.round(((temperature - 273.15) * 1.8) + 32)
+    }
 
     // Use the [OpenWeather API](https://openweathermap.org/api)
     //- http://api.openweathermap.org/data/2.5/weather
@@ -13,11 +15,18 @@ $(document).ready(function () {
     let forecastURL = "http://api.openweathermap.org/data/2.5/forecast?appid=" + apiKey + "&q=";
 
 
-
-
     $("#seachBtn").on("click", function () {
         let city = $("#city").val()
-        let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + city;
+        // Write a local item..
+     function store(item_ids) {
+    var items = [];
+    for(var i = 0; i < item_ids.length; i++) {
+        items.push(item_ids[i]);
+    }
+    localStorage.setItem("item", JSON.stringify(items));
+}
+console.log(localStorage.city)
+        let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + city+"&units=imperial";
         $.ajax({
             url: weatherURL,
             method: "GET"
@@ -31,39 +40,88 @@ $(document).ready(function () {
             img.attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
             var h3 = $("<h3>")
             h3.append(response.name, " (" + currentDate + ")", img)
-
             var p = $("<p>")
-            temp = ((response.main.temp - 273.15) * 1.8) + 32
-            temp = Math.round(temp)
+            var temp=response.main.temp
             p.append("temperature: " + temp + " F" + "<br><br>")
             var humidity = response.main.humidity
-            p.append("humidity: " + humidity)
+            p.append("humidity: " + humidity + "<br></br>")
             col.append(h3, p)
             row.html(col)
-            $("#dashboard").append(row)
+            $("#dashboard").prepend(row)
+            var windSpeed = response.wind.speed
+            p.append("wind speed: " + windSpeed + "<br></br>")
+            
+
+
+
+
 
             var lat = response.coord.lat
             var lon = response.coord.lon
             $.ajax({
                 url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon,
                 method: "GET"
-            }).then(function (response2) {
+            }).then(function (response2 ) {
+                 const uvIndex = response2.value
+            p.append("uv: " + uvIndex + "<br></br>")
+                console.log(response.value)
                 console.log("response2:")
                 console.log(response2)
+                   
+
 
                 $.ajax({
-                    url: "http://api.openweathermap.org/data/2.5/forecast?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon + "&q=",
+                    url: "http://api.openweathermap.org/data/2.5/forecast?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon + "&units=imperial",
                     method: "GET"
-                }).then(function(response3){
-                    console.log("response3:")
+                }).then(function (response3) {
+                    var forecasts=response3.list
+                    $(".forecast-row").empty()
+                    for (let i=1; i<forecasts.length && i<6; i++){
+                        
+                        $(".forecast-row").append(`<div class='col weather-future' data-id=${i}>Temperature: ${Math.round(forecasts[i].main.temp)}</div>
+                        <img src=${`http://openweathermap.org/img/w/${forecasts[i].weather[0].icon}.png`}></img>
+                        `)
+                    }
+                        
+                    })
+                    })
                     console.log(response3)
+
+              $('#forecast').empty();
+                
+                    var colOne = $("<div class='col-sm-2'>")
+                     var rowOne = $("<div class='row'>")
+                    
+                    
+                    
+
+                     $("#forecast").append(rowOne).append(colOne)
                 })
+
+
+
+
+
+
+                //API call:5 day forcast
+                // api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+                // Parameters:
+                // q city name and country code divided by comma, use ISO 3166 country codes
+
+                // Examples of API calls:
+                // api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml
+
+
+
+
+
+
             })
 
 
         })
 
-    })
+    
 
 
 
@@ -97,4 +155,3 @@ $(document).ready(function () {
 
 
 
-})
